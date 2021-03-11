b7db6302a5dc0470a59af35b7e7da4872a377c5b
ExplosiveGreen <69218745+ExplosiveGreen@users.noreply.github.com>
2021-03-11 15:27:26 +0200

    Update snowman.hpp

--- snowman.hpp
+++ snowman.hpp
@@ -1,4 +1,25 @@
 #include<string>
+#define NUMLETTERS 8
+#define MAXCHOISE 4
+#define DIV 10
+#define MINCHOISE 1
+#define HUP 0
+#define N 1
+#define L 2
+#define R 3
+#define X 4
+#define Y 5
+#define T 6
+#define B 7
+#define HDOWN 8
+#define NOUP 1
+#define NOLEFT 4
+#define LEFTUP 2
+#define LEFTDOWN1 1
+#define LEFTDOWN2 3
+#define MOD 2
+#define ZUGI 0
+#define IZUGI 1
 using namespace std;
 namespace ariel{
     string snowman(int number);
